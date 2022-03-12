fd3ccaaceda80cf43119f8aaebf6062cb55de5cd
vera.sprinceana <sprinceana.vera96@gmail.com>
2022-03-12 12:11:42 +0100

    finish

--- asset/javascript/main.js
+++ asset/javascript/main.js
@@ -1,22 +1,31 @@
 //Scrivi un programma che stampi in console i numeri da 1 a 100.
-for(let i=1; i<=100; i++){
+//MILESTONE 1
+//Per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi Buzz.
+//Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
+for( i=1; i<=100; i++){
     console.log (i);
-    if((i % 3 ==0)&& (i % 5==0)){
-        console.log("`${i}`FizzBuzz");  
-    }else if(i % 3 ==0){
-        console.log("`${i}`Fizz");
-        }else if(i % 5 ==0){
-        console.log("`${i}`Buzz");
+   
+}
+for(i=1; i<=100; i++){
+    if((i%3==0) && (i%5==0)){
+        console.log ("FizzBuzz")
+        document.getElementById("containerNumeri").innerHTML +=`<div class="numeri FizzBuzz"> FizzBuzz </div>`;    
+    }
+    else if(i%3==0){
+        console.log("Fizz")
+        document.getElementById("containerNumeri").innerHTML +=`<div class="numeri Fizz"> Fizz </div>`;    
+    }else if(i%5==0){
+        console.log("Buzz")
+        document.getElementById("containerNumeri").innerHTML +=`<div class="numeri Buzz"> Buzz </div>`;    
     }else{
-        console.log();
+       console.log(i) 
+       document.getElementById("containerNumeri").innerHTML +=`<div class="numeri"> ${i} </div>`;    
     }
     
 }
 
-
-//MILESTONE 1
-//Per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi Buzz.
-//Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
+    
+   
 
 //MILESTONE 2 (Bonus)
 //Dato un container nel DOM, appendi un elemento html con il numero o la stringa corretta.
